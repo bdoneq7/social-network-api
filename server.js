4321cba8e47fdd5ce364881606ecaab06e15b0eb
Brian Done <briguy2078@gmail.com>
2022-03-08 12:49:59 -0500

    updated server file

--- server.js
+++ server.js
@@ -6,17 +6,16 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static('public'));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
-  // useFindAndModify: false,
+  useFindAndModify: false,
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
 // Use this to log mongo queries being executed!
 mongoose.set('debug', true);
-
-app.use(require('./routes'));
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
