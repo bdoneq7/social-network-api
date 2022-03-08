1ba57c8d8f75d0a5fc8bf951add7ef75f35e8da7
Brian Done <briguy2078@gmail.com>
2022-03-08 14:28:47 -0500

    updated index models

--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
-const User = require('./User');
-const Thought = require('./Thoughts');
+const Users = require('./Users');
+const Thoughts = require('./Thoughts');
 
-module.exports = { User, Thoughts };
+module.exports = { Users, Thoughts };
